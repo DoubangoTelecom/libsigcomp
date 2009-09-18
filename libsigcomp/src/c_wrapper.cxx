94d36927ca5c289c02b3dcc61f516928b2027175
bossiel <bossiel@yahoo.fr>
2009-09-18 09:26:55 +0000

    Add pure "C" wrapper.

---
+++ libsigcomp/src/c_wrapper.cxx
@@ -0,0 +1,166 @@
+/**
+	This file is part of libSigComp project.
+
+	libSigComp is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libSigComp is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libSigComp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Copyright (C) 2008 Inexbee
+*
+* Authors:	Mamadou Diop <mamadou.diop@inexbee.com>
+*			Laurent Etiemble <laurent.etiemble@inexbee.com>
+*/
+
+#include "c_wrapper.h"
+
+#define GET_MANAGER(manager) ((sigcomp::SigCompManager*)manager)
+#define GET_RESULT(result) ((sigcomp::DecompressionResult*)result)
+
+/**
+	new SigCompManager()
+	Returns 1 if success, otherwise 0
+*/
+int SigCompManager_new(SigCompManager** manager)
+{
+	*manager = (SigCompManager*)new sigcomp::SigCompManager();
+	return *manager?1:0;
+}
+
+/**
+	inline SigCompManager::addSipSdpDictionary()
+*/
+void SigCompManager_addSipSdpDictionary(SigCompManager* manager)
+{
+	GET_MANAGER(manager)->addSipSdpDictionary();
+}
+
+/**
+	inline SigCompManager::addPresenceDictionary();
+*/
+void SigCompManager_addPresenceDictionary(SigCompManager* manager)
+{
+	GET_MANAGER(manager)->addPresenceDictionary();
+}
+
+/**
+	inline void SigCompManager::setDecompression_Memory_Size(uint32_t dms)
+*/
+void SigCompManager_setDecompression_Memory_Size(SigCompManager* manager, uint32_t dms)
+{
+	GET_MANAGER(manager)->setDecompression_Memory_Size(dms);
+}
+
+/**
+	inline  void setState_Memory_Size(uint32_t sms)
+*/
+void SigCompManager_setState_Memory_Size(SigCompManager* manager, uint32_t sms)
+{
+	GET_MANAGER(manager)->setState_Memory_Size(sms);
+}
+
+/**
+	inline void setCycles_Per_Bit(uint8_t cpb)
+*/
+void SigCompManager_setCycles_Per_Bit(SigCompManager* manager, uint8_t cpb)
+{
+	GET_MANAGER(manager)->setCycles_Per_Bit(cpb);
+}
+
+/**
+	size_t SigCompManager::SigCompManager::compress(uint64_t compartmentId, LPCVOID input_ptr, size_t input_size, LPVOID output_ptr, size_t output_size, bool stream)
+*/
+size_t SigCompManager_compress(SigCompManager* manager, uint64_t compartmentId, const void* input_ptr, size_t input_size, void* output_ptr, size_t output_size, BOOLEAN stream)
+{
+	return GET_MANAGER(manager)->compress(compartmentId, input_ptr, input_size, output_ptr, output_size, stream);
+}
+
+/**
+	size_t SigCompManager::decompress(LPCVOID input_ptr, size_t input_size, lpDecompressionResult lpResult)
+*/
+size_t SigCompManager_decompress(SigCompManager* manager, const void* input_ptr, size_t input_size, struct DecompressionResult* lpResult)
+{
+	return GET_MANAGER(manager)->decompress(input_ptr, input_size, GET_RESULT(lpResult));
+}
+
+/**
+	inline void SigCompManager::provideCompartmentId(lpDecompressionResult lpResult)
+*/
+void SigCompManager_provideCompartmentId(SigCompManager* manager, struct DecompressionResult* lpResult)
+{
+	GET_MANAGER(manager)->provideCompartmentId(GET_RESULT(lpResult));
+}
+
+/**
+	SigCompManager::closeCompartment(...)
+*/
+void SigCompManager_closeCompartment(SigCompManager* manager, uint64_t compartmentId)
+{
+	GET_MANAGER(manager)->closeCompartment(compartmentId);
+}
+
+/**
+	delete SigCompManager
+*/
+void SigCompManager_delete(SigCompManager** manager)
+{
+	SAFE_DELETE_PTR(*manager);
+}
+
+
+
+
+
+
+
+
+
+/**
+	new DecompressionResult()
+	Returns 1 if success, otherwise 0
+*/
+int DecompressionResult_new(struct DecompressionResult** result)
+{
+	*result = (struct DecompressionResult*)new sigcomp::DecompressionResult();
+	return *result?1:0;
+}
+
+/**
+	inline void DecompressionResult::setCompartmentId(uint64_t id)
+*/
+void DecompressionResult_setCompartmentId(struct DecompressionResult* result, uint64_t compartmentId)
+{
+	GET_RESULT(result)->setCompartmentId(compartmentId);
+}
+
+/**
+	inline void DecompressionResult::setOutputBuffer(LPVOID output_ptr, size_t output_size, bool stream, uint64_t streamId=0)
+*/
+void DecompressionResult_setOutputBuffer(struct DecompressionResult* result, void* output_ptr, size_t output_size, BOOLEAN stream, uint64_t streamId)
+{
+	GET_RESULT(result)->setOutputBuffer(output_ptr, output_size, stream,  streamId);
+}
+/**
+	inline bool getIsNack()
+*/
+BOOLEAN DecompressionResult_getIsNack(struct DecompressionResult* result)
+{
+	return GET_RESULT(result)->getIsNack();
+}
+
+/**
+	delete result
+*/
+void DecompressionResult_delete(struct DecompressionResult** result)
+{
+	SAFE_DELETE_PTR(*result);
+}
---
+++ libsigcomp/src/c_wrapper.h
@@ -0,0 +1,69 @@
+/**
+	This file is part of libSigComp project.
+
+	libSigComp is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libSigComp is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libSigComp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* Copyright (C) 2008 Inexbee
+*
+* Authors:	Mamadou Diop <mamadou.diop@inexbee.com>
+*			Laurent Etiemble <laurent.etiemble@inexbee.com>
+*/
+
+#ifndef _C_WRAPPER_H_
+#define _C_WRAPPER_H_
+
+#include "MYTYPES.h"
+
+#ifdef __cplusplus
+#	include "libsigcomp_config.h"
+#	include "SigCompManager.h"
+#endif
+
+#ifdef __cplusplus
+#	define EXTERN_C	extern "C" LIBSIGCOMP_API
+#	define BOOLEAN	bool
+#else
+#	define EXTERN_C extern
+#	define BOOLEAN	short
+#endif
+
+#ifndef __cplusplus
+typedef void SigCompManager;
+typedef void DecompressionResult;
+#endif
+
+EXTERN_C int SigCompManager_new(SigCompManager**);
+EXTERN_C void SigCompManager_addSipSdpDictionary(SigCompManager*);
+EXTERN_C void SigCompManager_addPresenceDictionary(SigCompManager*);
+EXTERN_C void SigCompManager_setDecompression_Memory_Size(SigCompManager*, uint32_t);
+EXTERN_C void SigCompManager_setState_Memory_Size(SigCompManager*, uint32_t);
+EXTERN_C void SigCompManager_setCycles_Per_Bit(SigCompManager* manager, uint8_t cpb);
+EXTERN_C size_t SigCompManager_compress(SigCompManager*, uint64_t compartmentId, const void* input_ptr, size_t input_size, void* output_ptr, size_t output_size, BOOLEAN stream);
+EXTERN_C size_t SigCompManager_decompress(SigCompManager*, const void* input_ptr, size_t input_size, struct DecompressionResult* );
+EXTERN_C void SigCompManager_provideCompartmentId(SigCompManager* , struct DecompressionResult* );
+EXTERN_C void SigCompManager_closeCompartment(SigCompManager*, uint64_t compartmentId);
+EXTERN_C void SigCompManager_delete(SigCompManager**);
+
+
+EXTERN_C int DecompressionResult_new(struct DecompressionResult**);
+EXTERN_C void DecompressionResult_setCompartmentId(struct DecompressionResult*, uint64_t compartmentId);
+EXTERN_C void DecompressionResult_setOutputBuffer(struct DecompressionResult*, void* output_ptr, size_t output_size, BOOLEAN stream, uint64_t streamId);
+EXTERN_C BOOLEAN DecompressionResult_getIsNack(struct DecompressionResult* );
+EXTERN_C void DecompressionResult_delete(struct DecompressionResult**);
+
+//#undef BOOLEAN
+#undef EXTERN_C
+
+#endif //_C_WRAPPER_H__
